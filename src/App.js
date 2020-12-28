b5a436e76d8227f2466ca76876de42ebea457bd8
Stepan <nemail.necom@gmail.com>
2020-12-29 01:34:51 +0300

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react';
+import s from  './App.module.css';
 
 function App() {
+
+    const initialState = {info: 'Hello World!'};
+    function reducer(state = initialState, action) {
+        switch (action.type) {
+            case 'about':
+                return {
+                    ...state,
+                    info: 'My name is Steve Nebykov'
+                };
+            case 'projects':
+                return {
+                    ...state,
+                    info: 'There is link to my github: '
+                };
+            case 'contacts':
+                return {
+                    ...state,
+                    info: 'Twitter: '
+                };
+            default:
+                return state;
+        }
+    }
+    const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.main}>
+        <div className={s.menu}>
+
+            <div className={s.section} onClick={() => dispatch({type: 'about'})}>
+                <h2>About me</h2>
+            </div>
+
+            <div className={s.section} onClick={() => dispatch({type: 'projects'})}>
+                <h2>Projects</h2>
+            </div>
+
+            <div className={s.section} onClick={() => dispatch({type: 'contacts'})}>
+                <h2>Contacts</h2>
+            </div>
+        </div>
+
+        <div className={s.info}>
+            <p> {state.info} </p>
+        </div>
+
     </div>
   );
 }
